deed4a6ee1186c79f140d52cb8e3a5edee584f1c
Quinten Palmer <quinten@clypd.com>
2017-04-18 18:13:26 -0400

    Use Html.Program as main for elm app
    This allows us to retrieve data and update the app with its contents.
    This will certainly evolve over time, but starting with a skeleton of
    something useful here.
    
    (*) See http://elm-lang.org/examples/http for the reference this was
    based off of.
    
    (*) http://package.elm-lang.org/packages/elm-lang/core/5.1.1/Json-Decode
    is a useful resource as well.

--- src/pathwebclient/Main.elm
+++ src/pathwebclient/Main.elm
@@ -4,6 +4,68 @@ import Html exposing (div)
 import Html.Attributes as Attr
 import Css
 
+import Json.Decode as Decode
+
+import Http
+
+
+main =
+    Html.program
+        { init = init "Traveler"
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { name : String
+    }
+
+
+init : String -> ( Model, Cmd Msg )
+init name =
+    ( Model name
+    , getCharacterSheet name
+    )
+
+
+type Msg
+    = DoLoadSheet
+    | SheetLoaded (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        DoLoadSheet ->
+            ( model, getCharacterSheet model.name )
+
+        SheetLoaded (Ok name) ->
+            ( Model name, Cmd.none )
+
+        SheetLoaded (Err _) ->
+            ( model, Cmd.none )
+
+
+getCharacterSheet : String -> Cmd Msg
+getCharacterSheet name =
+    let
+        url =
+            "http://localhost:3000/?name=" ++ name
+    in
+        Http.send SheetLoaded (Http.get url decodeCharacterResp)
+
+
+decodeCharacterResp : Decode.Decoder String
+decodeCharacterResp =
+    Decode.at [ "data", "name" ] Decode.string
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 
 cssStyle =
     Css.asPairs >> Attr.style
@@ -49,7 +111,7 @@ h1 =
         ]
 
 
-main =
+view model =
     div
         [ fullPage
         ]
@@ -58,12 +120,12 @@ main =
             ]
             [ Html.text "Pathfinder Character Sheet" ]
         , div [ content ]
-            [ innerPage ]
+            [ innerPage model ]
         ]
 
 
-innerPage =
+innerPage model =
     div []
         [ Html.div [ h1 ] [ Html.text "Welcome!" ]
-        , Html.div [ p ] [ Html.text "Hello! I'm sorry, I don't know your name yet." ]
+        , Html.div [ p ] [ Html.text <| "Hello, " ++ model.name ++ "! Good to see ya!" ]
         ]

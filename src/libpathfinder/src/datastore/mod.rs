a3fe7460dcc7795b07874bb153b60a85a54bbdf2
Quinten Palmer <quinten@clypd.com>
2017-04-29 18:01:06 -0400

    Prefer to use error::Error in datastore logic

--- src/libpathfinder/src/datastore/mod.rs
+++ src/libpathfinder/src/datastore/mod.rs
@@ -3,8 +3,8 @@ use std::path;
 use rusqlite;
 
 use libpathfinder_common::FromDB;
+use libpathfinder_common::error;
 
-use libpathfinder_common::error::Error;
 use models;
 
 pub struct Datastore {
@@ -12,12 +12,12 @@ pub struct Datastore {
 }
 
 impl Datastore {
-    pub fn new<P: AsRef<path::Path>>(p: P) -> Result<Datastore, Error> {
-        let conn = try!(rusqlite::Connection::open(p).map_err(Error::Rusqlite));
+    pub fn new<P: AsRef<path::Path>>(p: P) -> Result<Datastore, error::Error> {
+        let conn = try!(rusqlite::Connection::open(p).map_err(error::Error::Rusqlite));
         return Ok(Datastore { conn: conn });
     }
 
-    pub fn get_character(&self, id: u32) -> Result<models::Character, Error> {
+    pub fn get_character(&self, id: u32) -> Result<models::Character, error::Error> {
         models::Character::select_one(&self.conn, id)
     }
 }

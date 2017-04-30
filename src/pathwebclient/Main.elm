09ce21f7e2249f7897486ca89a5742e29e17d2d9
Quinten Palmer <quinten@clypd.com>
2017-04-30 13:49:59 -0400

    Consume and display ability scores for character
    This:
        * Adds the ability score information to the model of the elm app
        * Parses the ability score information from the json payload coming
            from the backend
        * Displays the ability score information, including:
            * the ability names
            * the ability scores themselves
            * the ability modifiers
            * the ability modifiers as emoji counters
    
    Including table css to help make the table prettier and easier to read.

--- src/pathwebclient/Main.elm
+++ src/pathwebclient/Main.elm
@@ -19,12 +19,28 @@ main =
 type alias Model =
     { id : Int
     , name : String
+    , abilityScores : AbilityScoreSet
     }
+
+
+type alias AbilityScoreSet =
+    { str : Int
+    , dex : Int
+    , con : Int
+    , int : Int
+    , wis : Int
+    , cha : Int
+    }
+
+
+emptyAbilityScoreSet : AbilityScoreSet
+emptyAbilityScoreSet =
+    AbilityScoreSet 0 0 0 0 0 0
 
 
 init : String -> ( Model, Cmd Msg )
 init name =
-    ( Model 0 name
+    ( Model 0 name emptyAbilityScoreSet
     , getCharacterSheet 1
     )
 
@@ -44,7 +60,7 @@ update msg model =
             ( newModel, Cmd.none )
 
         SheetLoaded (Err _) ->
-            ( Model 0 "ErrorCauser", Cmd.none )
+            ( Model 0 "ErrorCauser" emptyAbilityScoreSet, Cmd.none )
 
 
 getCharacterSheet : Int -> Cmd Msg
@@ -58,7 +74,21 @@ getCharacterSheet id =
 
 decodeCharacterResp : Decode.Decoder Model
 decodeCharacterResp =
-    Decode.map2 Model (Decode.at [ "data", "id" ] Decode.int) (Decode.at [ "data", "name" ] Decode.string)
+    Decode.map3 Model
+        (Decode.at [ "data", "id" ] Decode.int)
+        (Decode.at [ "data", "name" ] Decode.string)
+        (Decode.at [ "data", "ability_scores" ] decodeAbilityScores)
+
+
+decodeAbilityScores : Decode.Decoder AbilityScoreSet
+decodeAbilityScores =
+    Decode.map6 AbilityScoreSet
+        (Decode.field "str" Decode.int)
+        (Decode.field "dex" Decode.int)
+        (Decode.field "con" Decode.int)
+        (Decode.field "int" Decode.int)
+        (Decode.field "wis" Decode.int)
+        (Decode.field "cha" Decode.int)
 
 
 subscriptions : Model -> Sub Msg
@@ -110,6 +140,20 @@ h1 =
         ]
 
 
+table =
+    cssStyle
+        [ Css.width (Css.px 350)
+        , Css.border (Css.px 2)
+        ]
+
+
+tableData =
+    cssStyle
+        [ Css.border3 (Css.px 1) Css.solid (Css.rgb 200 200 200)
+        , Css.padding (Css.px 3)
+        ]
+
+
 view model =
     div
         [ fullPage
@@ -127,4 +171,57 @@ innerPage model =
     div []
         [ Html.div [ h1 ] [ Html.text "Welcome!" ]
         , Html.div [ p ] [ Html.text <| "Hello, " ++ model.name ++ "! Good to see ya!" ]
+        , Html.div []
+            [ Html.table [ table ]
+                [ Html.tr []
+                    [ Html.th [] [ Html.text "Ability Name" ]
+                    , Html.th [] [ Html.text "Score" ]
+                    , Html.th [] [ Html.text "Modifier" ]
+                    , Html.th [] [ Html.text "EModji" ]
+                    ]
+                , scoreTableRow "STR" model.abilityScores.str "🐂"
+                , scoreTableRow "DEX" model.abilityScores.dex "🐆"
+                , scoreTableRow "CON" model.abilityScores.con "🐎"
+                , scoreTableRow "INT" model.abilityScores.int "\x1F991"
+                , scoreTableRow "WIS" model.abilityScores.wis "\x1F989"
+                , scoreTableRow "CHA" model.abilityScores.cha "🎭"
+                ]
+            ]
         ]
+
+
+scoreTableRow : String -> Int -> String -> Html.Html Msg
+scoreTableRow name val emoji =
+    let
+        mod =
+            calcAbilityModifier val
+    in
+        Html.tr []
+            [ htmlTdStr name
+            , htmlTdStr (toString val)
+            , htmlTdStr (toString <| mod)
+            , htmlTdStr
+                <| if mod >= 0 then
+                    String.repeat (mod) emoji
+                   else
+                    "➖" ++ String.repeat (-mod) emoji
+            ]
+
+
+htmlTdStr s =
+    Html.td [ tableData ]
+        [ Html.text s ]
+
+
+calcAbilityModifier : Int -> Int
+calcAbilityModifier i =
+    let
+        rounded =
+            if i % 2 == 0 then
+                i
+            else if i > 0 then
+                i - 1
+            else
+                i + 1
+    in
+        (rounded - 10) // 2

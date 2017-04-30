abf70cc439741a41081d09f49d298d156edd0c12
Quinten Palmer <quinten@clypd.com>
2017-04-29 22:34:52 -0400

    Don't seed into existing database
    If there is already a database file to read into, don't try to restore
    the schema and data into the file, which presumable already has that
    data.

--- src/libpathfinder/db/seed.sh
+++ src/libpathfinder/db/seed.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ -e local_pf.db ]; then
+    echo "you already have a local_pf.db file, remove it first"
+    exit
+fi
 sqlite3 local_pf.db "$(cat schema.sql)"
 sqlite3 local_pf.db "$(cat bootstrap.sql)"

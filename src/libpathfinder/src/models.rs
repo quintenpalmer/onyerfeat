5f1ff6dd703dd9bd6e1b5fecb69339a936bc7e5b
Quinten Palmer <quinten@clypd.com>
2017-04-29 22:06:23 -0400

    Prefer public fields to constructor for Character
    This will flow better for construction, and we don't need to hide these
    fields, so preferring to let people callers Characters with the fields.

--- src/libpathfinder/src/models.rs
+++ src/libpathfinder/src/models.rs
@@ -4,15 +4,6 @@ use libpathfinder_common::error;
 #[derive(Serialize, Deserialize, FromDB)]
 #[from_db(table_name = "characters")]
 pub struct Character {
-    id: u32,
-    name: String,
-}
-
-impl Character {
-    pub fn new(id: u32, name: String) -> Character {
-        return Character {
-            id: id,
-            name: name,
-        };
-    }
+    pub id: u32,
+    pub name: String,
 }

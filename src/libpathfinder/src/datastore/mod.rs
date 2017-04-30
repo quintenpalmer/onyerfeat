2f0c0f2babea48531e5d14963cfeb5bf00139501
Quinten Palmer <quinten@clypd.com>
2017-04-29 23:00:34 -0400

    Introduce ability scores on the backend
    This goes through the motions of:
        * setting up the data in the local environment (saving the new
            schema and data)
        * introducing the db struct to match the sqlite table
        * introducing the canonical model to serialize out to json
        * converting from the db struct to the canonical model and attaching
            this information to the character model
    
    Would like to display this in the UI next, fingers crossed it goes well!

--- src/libpathfinder/src/datastore/mod.rs
+++ src/libpathfinder/src/datastore/mod.rs
@@ -12,6 +12,19 @@ use models;
 struct Character {
     id: u32,
     name: String,
+    ability_score_set_id: u32,
+}
+
+#[derive(FromDB)]
+#[from_db(table_name = "ability_score_sets")]
+struct AbilityScoreSet {
+    id: u32,
+    str: u32,
+    dex: u32,
+    con: u32,
+    int: u32,
+    wis: u32,
+    cha: u32,
 }
 
 pub struct Datastore {
@@ -26,9 +39,18 @@ impl Datastore {
 
     pub fn get_character(&self, id: u32) -> Result<models::Character, error::Error> {
         let c = try!(Character::select_one(&self.conn, id));
+        let abs = try!(AbilityScoreSet::select_one(&self.conn, c.ability_score_set_id));
         return Ok(models::Character {
             id: c.id,
             name: c.name,
+            ability_scores: models::AbilityScoreSet {
+                str: abs.str,
+                dex: abs.dex,
+                con: abs.con,
+                int: abs.int,
+                wis: abs.wis,
+                cha: abs.cha,
+            },
         });
     }
 }
--- src/libpathfinder/src/models.rs
+++ src/libpathfinder/src/models.rs
@@ -2,4 +2,15 @@
 pub struct Character {
     pub id: u32,
     pub name: String,
+    pub ability_scores: AbilityScoreSet,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AbilityScoreSet {
+    pub str: u32,
+    pub dex: u32,
+    pub con: u32,
+    pub int: u32,
+    pub wis: u32,
+    pub cha: u32,
 }

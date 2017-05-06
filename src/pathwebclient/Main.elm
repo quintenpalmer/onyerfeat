a2c9390fafbc975c93527edd19c7363ef6ec6010
Quinten Palmer <quinten@clypd.com>
2017-05-05 20:30:08 -0400

    Extract Decoding module out

--- src/pathwebclient/Main.elm
+++ src/pathwebclient/Main.elm
@@ -1,10 +1,10 @@
 module Main exposing (main)
 
 import Html
-import Json.Decode as Decode
 import Http
 import Models
 import View
+import Decoding
 
 
 main =
@@ -47,40 +47,7 @@ getCharacterSheet id =
         url =
             "http://localhost:3000/character?id=" ++ (toString id)
     in
-        Http.send SheetLoaded (Http.get url decodeCharacterResp)
-
-
-decodeCharacterResp : Decode.Decoder Models.Character
-decodeCharacterResp =
-    Decode.field "data" decodeCharacter
-
-
-decodeCharacter : Decode.Decoder Models.Character
-decodeCharacter =
-    Decode.map5 Models.Character
-        (Decode.field "id" Decode.int)
-        (Decode.field "name" Decode.string)
-        (Decode.field "player_name" Decode.string)
-        (Decode.field "ability_scores" decodeAbilityScores)
-        (Decode.field "alignment" decodeAlignment)
-
-
-decodeAbilityScores : Decode.Decoder Models.AbilityScoreSet
-decodeAbilityScores =
-    Decode.map6 Models.AbilityScoreSet
-        (Decode.field "str" Decode.int)
-        (Decode.field "dex" Decode.int)
-        (Decode.field "con" Decode.int)
-        (Decode.field "int" Decode.int)
-        (Decode.field "wis" Decode.int)
-        (Decode.field "cha" Decode.int)
-
-
-decodeAlignment : Decode.Decoder Models.Alignment
-decodeAlignment =
-    Decode.map2 Models.Alignment
-        (Decode.field "morality" Decode.string)
-        (Decode.field "order" Decode.string)
+        Http.send SheetLoaded (Http.get url Decoding.decodeCharacterResp)
 
 
 subscriptions : Models.Model -> Sub Msg

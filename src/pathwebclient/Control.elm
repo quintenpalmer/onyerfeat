f50359f94737061ef5b1e8a40e732c17b45e58ec
Quinten Palmer <quinten@clypd.com>
2017-05-05 20:35:16 -0400

    Extract Control module
    This may get split further into more modules, content with this logic
    all living together for now, though.

---
+++ src/pathwebclient/Control.elm
@@ -0,0 +1,40 @@
+module Control exposing (init, update, subscriptions)
+
+import Http
+import Models
+import Common
+import Decoding
+
+
+init : ( Models.Model, Cmd Common.Msg )
+init =
+    ( Models.MNotLoaded
+    , getCharacterSheet 1
+    )
+
+
+update : Common.Msg -> Models.Model -> ( Models.Model, Cmd Common.Msg )
+update msg character =
+    case msg of
+        Common.DoLoadSheet ->
+            ( character, getCharacterSheet 1 )
+
+        Common.SheetLoaded (Ok newCharacter) ->
+            ( Models.MCharacter newCharacter, Cmd.none )
+
+        Common.SheetLoaded (Err e) ->
+            ( Models.MError <| "Error loading sheet: " ++ toString e, Cmd.none )
+
+
+getCharacterSheet : Int -> Cmd Common.Msg
+getCharacterSheet id =
+    let
+        url =
+            "http://localhost:3000/character?id=" ++ (toString id)
+    in
+        Http.send Common.SheetLoaded (Http.get url Decoding.decodeCharacterResp)
+
+
+subscriptions : Models.Model -> Sub Common.Msg
+subscriptions model =
+    Sub.none
--- src/pathwebclient/Main.elm
+++ src/pathwebclient/Main.elm
@@ -1,51 +1,14 @@
 module Main exposing (main)
 
 import Html
-import Http
-import Common
-import Models
+import Control
 import View
-import Decoding
 
 
 main =
     Html.program
-        { init = init
+        { init = Control.init
         , view = View.view
-        , update = update
-        , subscriptions = subscriptions
+        , update = Control.update
+        , subscriptions = Control.subscriptions
         }
-
-
-init : ( Models.Model, Cmd Common.Msg )
-init =
-    ( Models.MNotLoaded
-    , getCharacterSheet 1
-    )
-
-
-update : Common.Msg -> Models.Model -> ( Models.Model, Cmd Common.Msg )
-update msg character =
-    case msg of
-        Common.DoLoadSheet ->
-            ( character, getCharacterSheet 1 )
-
-        Common.SheetLoaded (Ok newCharacter) ->
-            ( Models.MCharacter newCharacter, Cmd.none )
-
-        Common.SheetLoaded (Err e) ->
-            ( Models.MError <| "Error loading sheet: " ++ toString e, Cmd.none )
-
-
-getCharacterSheet : Int -> Cmd Common.Msg
-getCharacterSheet id =
-    let
-        url =
-            "http://localhost:3000/character?id=" ++ (toString id)
-    in
-        Http.send Common.SheetLoaded (Http.get url Decoding.decodeCharacterResp)
-
-
-subscriptions : Models.Model -> Sub Common.Msg
-subscriptions model =
-    Sub.none

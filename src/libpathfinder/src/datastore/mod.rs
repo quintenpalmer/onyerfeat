066ece76b25c9f2b9ea6d9f19af21b4b8ada72d1
Quinten Palmer <quintenpalmer@gmail.com>
2017-05-16 13:50:18 -0400

    Extract enum_impls out to separate module from structs
    These were polluting this module, so extracting them to their own
    module.

--- src/libpathfinder/src/datastore/mod.rs
+++ src/libpathfinder/src/datastore/mod.rs
@@ -2,5 +2,6 @@ mod structs;
 mod queries;
 mod concrete;
 mod selects;
+mod enum_impls;
 
 pub use self::concrete::Datastore;
--- src/libpathfinder/src/datastore/structs.rs
+++ src/libpathfinder/src/datastore/structs.rs
@@ -1,11 +1,10 @@
 use std::collections::{HashMap, HashSet};
-use std::str;
-use std::error as stderror;
 
-use models;
 use postgres;
 
 use libpathfinder_common::error;
+
+use models;
 
 #[derive(TableNamer)]
 #[table_namer(table_name = "characters")]
@@ -319,115 +318,6 @@ pub struct ClassSkillConstructor {
     pub id: i32,
     pub class_id: i32,
     pub skill_constructor_id: i32,
-}
-
-impl postgres::types::FromSql for models::AbilityName {
-    fn from_sql(ty: &postgres::types::Type,
-                raw: &[u8])
-                -> Result<Self, Box<stderror::Error + Send + Sync>> {
-        match ty {
-            &postgres::types::Type::Text => {
-                match try!(str::from_utf8(raw)) {
-                    "str" => Ok(models::AbilityName::Str),
-                    "dex" => Ok(models::AbilityName::Dex),
-                    "con" => Ok(models::AbilityName::Con),
-                    "int" => Ok(models::AbilityName::Int),
-                    "wis" => Ok(models::AbilityName::Wis),
-                    "cha" => Ok(models::AbilityName::Cha),
-                    _ => Err(Box::new(error::Error::ParseError {})),
-                }
-            }
-            _ => Err(Box::new(error::Error::ParseError {})),
-        }
-    }
-
-    fn accepts(ty: &postgres::types::Type) -> bool {
-        match ty {
-            &postgres::types::Type::Text => true,
-            _ => false,
-        }
-    }
-}
-
-impl postgres::types::FromSql for models::AlignmentOrder {
-    fn from_sql(ty: &postgres::types::Type,
-                raw: &[u8])
-                -> Result<Self, Box<stderror::Error + Send + Sync>> {
-        match ty {
-            &postgres::types::Type::Text => {
-                match try!(str::from_utf8(raw)) {
-                    "chaotic" => Ok(models::AlignmentOrder::Chaotic),
-                    "neutral" => Ok(models::AlignmentOrder::Neutral),
-                    "lawful" => Ok(models::AlignmentOrder::Lawful),
-                    _ => Err(Box::new(error::Error::ParseError {})),
-                }
-            }
-            _ => Err(Box::new(error::Error::ParseError {})),
-        }
-    }
-
-    fn accepts(ty: &postgres::types::Type) -> bool {
-        match ty {
-            &postgres::types::Type::Text => true,
-            _ => false,
-        }
-    }
-}
-
-impl postgres::types::FromSql for models::AlignmentMorality {
-    fn from_sql(ty: &postgres::types::Type,
-                raw: &[u8])
-                -> Result<Self, Box<stderror::Error + Send + Sync>> {
-        match ty {
-            &postgres::types::Type::Text => {
-                match try!(str::from_utf8(raw)) {
-                    "evil" => Ok(models::AlignmentMorality::Evil),
-                    "neutral" => Ok(models::AlignmentMorality::Neutral),
-                    "good" => Ok(models::AlignmentMorality::Good),
-                    _ => Err(Box::new(error::Error::ParseError {})),
-                }
-            }
-            _ => Err(Box::new(error::Error::ParseError {})),
-        }
-    }
-
-    fn accepts(ty: &postgres::types::Type) -> bool {
-        match ty {
-            &postgres::types::Type::Text => true,
-            _ => false,
-        }
-    }
-}
-
-impl postgres::types::FromSql for models::Size {
-    fn from_sql(ty: &postgres::types::Type,
-                raw: &[u8])
-                -> Result<Self, Box<stderror::Error + Send + Sync>> {
-        match ty {
-            &postgres::types::Type::Text => {
-                match try!(str::from_utf8(raw)) {
-                    "colossal" => Ok(models::Size::Colossal),
-                    "gargantuan" => Ok(models::Size::Gargantuan),
-                    "huge" => Ok(models::Size::Huge),
-                    "large" => Ok(models::Size::Large),
-                    "medium" => Ok(models::Size::Medium),
-                    "small" => Ok(models::Size::Small),
-                    "tiny" => Ok(models::Size::Tiny),
-                    "diminutive" => Ok(models::Size::Diminutive),
-                    "fine" => Ok(models::Size::Fine),
-                    _ => Err(Box::new(error::Error::ParseError {})),
-                }
-            }
-            _ => Err(Box::new(error::Error::ParseError {})),
-        }
-    }
-
-    fn accepts(ty: &postgres::types::Type) -> bool {
-        match ty {
-            &postgres::types::Type::Text => true,
-            _ => false,
-        }
-    }
 }
 
 fn calc_ability_modifier(i: i32) -> i32 {

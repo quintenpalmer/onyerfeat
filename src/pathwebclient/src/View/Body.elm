293aa4bcc3336c2ed50da890a26922b8c35ad5c4
Quinten Palmer <quintenpalmer@gmail.com>
2017-05-20 16:23:31 -0400

    Extract displayArmorPieces to separate module

--- src/pathwebclient/src/View/Body.elm
+++ src/pathwebclient/src/View/Body.elm
@@ -8,6 +8,7 @@ import Char
 import Common
 import Models
 import View.ShieldTable as ShieldTable
+import View.ArmorTable as ArmorTable
 
 
 cssStyle =
@@ -91,7 +92,7 @@ view model =
                     ShieldTable.displayShields ss
 
                 Models.MArmorPieces pieces ->
-                    displayArmorPieces pieces
+                    ArmorTable.displayArmorPieces pieces
 
                 Models.MDiceTab mRoll ->
                     displayDiceTab mRoll
@@ -451,45 +452,6 @@ innerPage character =
                         ]
                     ]
                 ]
-            ]
-        ]
-
-
-displayArmorPieces : List Models.ArmorPiece -> Html.Html Common.Msg
-displayArmorPieces armorPieces =
-    Html.div [ Attr.class "col-md-9" ]
-        [ Html.h2 [ Attr.class "text-center" ] [ Html.text "Armor" ]
-        , Html.table [ Attr.class "table table-striped table-bordered" ]
-            [ Html.thead []
-                [ Html.tr []
-                    [ Html.th [ Attr.class "text-center" ] [ Html.text "Armor Class" ]
-                    , Html.th [ Attr.class "text-center" ] [ Html.text "Name" ]
-                    , Html.th [ Attr.class "text-center" ] [ Html.text "Armor Bonus" ]
-                    , Html.th [ Attr.class "text-center" ] [ Html.text "Max Dex Bonus" ]
-                    , Html.th [ Attr.class "text-center" ] [ Html.text "Armor Check Penalty" ]
-                    , Html.th [ Attr.class "text-center" ] [ Html.text "Arcane Spell Failure Chance" ]
-                    , Html.th [ Attr.class "text-center" ] [ Html.text "Fast Speed" ]
-                    , Html.th [ Attr.class "text-center" ] [ Html.text "Slow Speed" ]
-                    , Html.th [ Attr.class "text-center" ] [ Html.text "Medium Weight" ]
-                    ]
-                ]
-            , Html.tbody [ Attr.class "text-center" ]
-                (List.map
-                    (\piece ->
-                        Html.tr []
-                            [ Html.td [] [ Html.b [] [ Html.text piece.armorClass ] ]
-                            , Html.td [] [ Html.b [] [ Html.text piece.name ] ]
-                            , Html.td [] [ Html.b [] [ Html.text <| toString piece.armorBonus ] ]
-                            , Html.td [] [ Html.b [] [ Html.text <| toString piece.maxDexBonus ] ]
-                            , Html.td [] [ Html.b [] [ Html.text <| toString piece.armorCheckPenalty ] ]
-                            , Html.td [] [ Html.b [] [ Html.text <| toString piece.arcaneSpellFailureChance ] ]
-                            , Html.td [] [ Html.b [] [ Html.text <| toString piece.fastSpeed ] ]
-                            , Html.td [] [ Html.b [] [ Html.text <| toString piece.slowSpeed ] ]
-                            , Html.td [] [ Html.b [] [ Html.text <| toString piece.mediumWeight ] ]
-                            ]
-                    )
-                    armorPieces
-                )
             ]
         ]
 

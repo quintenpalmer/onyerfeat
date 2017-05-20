b06a16bd92495af965b7bf53d7de91ebcbfcac68
Quinten Palmer <quintenpalmer@gmail.com>
2017-05-20 16:21:02 -0400

    Extract displayShield to separate module

--- src/pathwebclient/src/View/Body.elm
+++ src/pathwebclient/src/View/Body.elm
@@ -7,6 +7,7 @@ import Html.Events as Events
 import Char
 import Common
 import Models
+import View.ShieldTable as ShieldTable
 
 
 cssStyle =
@@ -87,7 +88,7 @@ view model =
                     Html.h1 [] [ Html.text "Loading" ]
 
                 Models.MShields ss ->
-                    displayShields ss
+                    ShieldTable.displayShields ss
 
                 Models.MArmorPieces pieces ->
                     displayArmorPieces pieces
@@ -447,58 +448,6 @@ innerPage character =
                                     character.skills
                                 )
                             ]
-                        ]
-                    ]
-                ]
-            ]
-        ]
-
-
-displayShields : List Models.Shield -> Html.Html Common.Msg
-displayShields shields =
-    Html.div [ Attr.class "container" ]
-        [ Html.div [ Attr.class "col-md-7", Attr.class "text-center" ]
-            [ Html.div [ Attr.class "panel panel-default" ]
-                [ Html.div [ Attr.class "panel-heading" ] [ Html.h3 [] [ Html.text "Shields" ] ]
-                , Html.div [ Attr.class "panel-body" ]
-                    [ Html.table [ Attr.class "table table-striped" ]
-                        [ Html.thead []
-                            [ Html.tr []
-                                [ Html.th [ Attr.class "text-center" ] [ Html.text "Name" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "AC Bonus" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Max Dex" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Skill Penalty" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Arcane Spell Failure Chance" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Weight" ]
-                                ]
-                            ]
-                        , Html.tbody [ Attr.class "text-center" ]
-                            (List.map
-                                (\shield ->
-                                    Html.tr []
-                                        [ Html.td [ Attr.class "text-left" ]
-                                            [ Html.text shield.name ]
-                                        , Html.td []
-                                            [ Html.text <| toString shield.acBonus ]
-                                        , Html.td []
-                                            [ Html.text <|
-                                                case shield.maxDex of
-                                                    Nothing ->
-                                                        "_"
-
-                                                    Just maxDex ->
-                                                        toString maxDex
-                                            ]
-                                        , Html.td []
-                                            [ Html.text <| toString shield.skillPenalty ]
-                                        , Html.td []
-                                            [ Html.text <| toString shield.arcaneSpellFailureChance ]
-                                        , Html.td []
-                                            [ Html.text <| toString shield.weight ]
-                                        ]
-                                )
-                                shields
-                            )
                         ]
                     ]
                 ]

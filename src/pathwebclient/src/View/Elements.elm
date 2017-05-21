903065cb528d8624c843ec7fbb34fb24b9f79a06
Quinten Palmer <quintenpalmer@gmail.com>
2017-05-20 21:04:44 -0400

    Correct text-align for panelled
    We weren't getting the text-align center that we always want on the
    heading. If the caller doesn't want text align for the body, we
    overwrite the text align to left for the body.

--- src/pathwebclient/src/View/Elements.elm
+++ src/pathwebclient/src/View/Elements.elm
@@ -10,16 +10,17 @@ import Common
 
 panelled : String -> Bool -> List (Html.Html Common.Msg) -> Html.Html Common.Msg
 panelled title center children =
-    Html.div
-        (if center then
-            [ Attr.class "text-center" ]
-         else
-            []
-        )
-    <|
+    Html.div [ Attr.class "text-center" ] <|
         [ Html.div [ Attr.class "panel panel-default" ]
             [ Html.div [ Attr.class "panel-heading panel-small-header" ] [ Html.text title ]
-            , Html.div [ Attr.class "panel-body" ]
+            , Html.div
+                ([ Attr.class "panel-body" ]
+                    ++ (if center then
+                            []
+                        else
+                            [ Attr.class "text-left" ]
+                       )
+                )
                 children
             ]
         ]

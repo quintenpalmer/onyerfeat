f5aa871de736099207724c2d61f02d4b8f961171
Quinten Palmer <quintenpalmer@gmail.com>
2017-05-21 20:20:09 -0400

    Extract weaponsPanel function to export
    The character sheet tab will use this to display character weapons.

--- src/pathwebclient/src/View/WeaponTable.elm
+++ src/pathwebclient/src/View/WeaponTable.elm
@@ -1,4 +1,4 @@
-module View.WeaponTable exposing (displayWeapons)
+module View.WeaponTable exposing (displayWeapons, weaponsPanel)
 
 import Html
 import Html.Attributes as Attr
@@ -14,44 +14,48 @@ displayWeapons weapons =
         , Html.div [ Attr.class "row" ]
             [ Html.div [ Attr.class "col-md-1" ] []
             , Html.div [ Attr.class "col-md-10" ]
-                [ Elements.panelled "Weapons Table"
-                    True
-                    [ Html.table [ Attr.class "table table-striped table-bordered" ]
-                        [ Html.thead []
-                            [ Html.tr []
-                                [ Html.th [ Attr.class "text-center" ] [ Html.text "Name" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Training Type" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Style" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Cost" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Damage (small)" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Damage (med)" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Critical" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Range" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Weight" ]
-                                , Html.th [ Attr.class "text-center" ] [ Html.text "Damage Type" ]
-                                ]
-                            ]
-                        , Html.tbody [ Attr.class "text-center" ]
-                            (List.map
-                                (\weapon ->
-                                    Html.tr []
-                                        [ Html.td [] [ Html.b [] [ Html.text weapon.name ] ]
-                                        , Html.td [] [ Html.b [ Attr.class "text-capitalize" ] [ Html.text weapon.trainingType ] ]
-                                        , Html.td [] [ Html.b [ Attr.class "text-capitalize" ] [ Html.text <| (String.join " " (String.split "_" weapon.sizeStyle)) ] ]
-                                        , Html.td [] [ Html.b [] [ Html.text <| (toString weapon.cost) ++ "gp" ] ]
-                                        , Html.td [] [ Html.b [] [ Html.text <| displayDice weapon.smallDamage ] ]
-                                        , Html.td [] [ Html.b [] [ Html.text <| displayDice weapon.mediumDamage ] ]
-                                        , Html.td [] [ Html.b [] [ Html.text <| displayCriticalDamage weapon.critical ] ]
-                                        , Html.td [] [ Html.b [] [ Html.text <| (toString weapon.range) ++ "ft" ] ]
-                                        , Html.td [] [ Html.b [] [ Html.text <| (toString weapon.weight) ++ "lbs" ] ]
-                                        , Html.td [] [ Html.b [] [ Html.text <| displayPhysicalDamage weapon.damageType ] ]
-                                        ]
-                                )
-                                weapons
-                            )
-                        ]
+                [ weaponsPanel weapons ]
+            ]
+        ]
+
+
+weaponsPanel : List Models.Weapon -> Html.Html Common.Msg
+weaponsPanel weapons =
+    Elements.panelled "Weapons Table"
+        True
+        [ Html.table [ Attr.class "table table-striped table-bordered" ]
+            [ Html.thead []
+                [ Html.tr []
+                    [ Html.th [ Attr.class "text-center" ] [ Html.text "Name" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Training Type" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Style" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Cost" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Damage (small)" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Damage (med)" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Critical" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Range" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Weight" ]
+                    , Html.th [ Attr.class "text-center" ] [ Html.text "Damage Type" ]
                     ]
                 ]
+            , Html.tbody [ Attr.class "text-center" ]
+                (List.map
+                    (\weapon ->
+                        Html.tr []
+                            [ Html.td [] [ Html.b [] [ Html.text weapon.name ] ]
+                            , Html.td [] [ Html.b [ Attr.class "text-capitalize" ] [ Html.text weapon.trainingType ] ]
+                            , Html.td [] [ Html.b [ Attr.class "text-capitalize" ] [ Html.text <| (String.join " " (String.split "_" weapon.sizeStyle)) ] ]
+                            , Html.td [] [ Html.b [] [ Html.text <| (toString weapon.cost) ++ "gp" ] ]
+                            , Html.td [] [ Html.b [] [ Html.text <| displayDice weapon.smallDamage ] ]
+                            , Html.td [] [ Html.b [] [ Html.text <| displayDice weapon.mediumDamage ] ]
+                            , Html.td [] [ Html.b [] [ Html.text <| displayCriticalDamage weapon.critical ] ]
+                            , Html.td [] [ Html.b [] [ Html.text <| (toString weapon.range) ++ "ft" ] ]
+                            , Html.td [] [ Html.b [] [ Html.text <| (toString weapon.weight) ++ "lbs" ] ]
+                            , Html.td [] [ Html.b [] [ Html.text <| displayPhysicalDamage weapon.damageType ] ]
+                            ]
+                    )
+                    weapons
+                )
             ]
         ]
 

1a9d4917c50c2d22697d929118987a498e103037
Quinten Palmer <quintenpalmer@gmail.com>
2018-03-03 22:33:08 -0500

    Update character health points

--- src/libpathfinder/db/data.sql
+++ src/libpathfinder/db/data.sql
@@ -138,9 +138,9 @@ COPY public.classes (id, name) FROM stdin;
 
 COPY public.creatures (id, name, ability_score_set_id, alignment_order, alignment_morality, race, deity, age, size, max_hit_points, current_hit_points, nonlethal_damage, base_attack_bonus, level, armor_piece_instance_id) FROM stdin;
 3	Datanamàsh	4	neutral	good	dwarf	\N	78	medium	51	51	0	5	5	2
-4	Atolabsam	5	lawful	good	dwarf	\N	80	medium	48	48	0	5	5	3
-1	IDRIGOTH	1	neutral	good	dwarf	\N	63	medium	85	85	0	6	6	1
 5	Charger	6	lawful	good	Horse	\N	10	large	36	36	0	3	5	4
+4	Atolabsam	5	lawful	good	dwarf	\N	80	medium	48	31	0	5	5	3
+1	IDRIGOTH	1	neutral	good	dwarf	\N	63	medium	85	42	0	6	6	1
 \.
 
 
